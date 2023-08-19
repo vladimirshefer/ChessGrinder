8e9f76f671ed8aefb0c1cbd661821fbd4b57caba
Vladimir Shefer <quameu@gmail.com>
2023-08-20 01:33:13 +0400

    UI: Fix eslint

--- frontend/src/pages/TournamentPage/RoundTable.tsx
+++ frontend/src/pages/TournamentPage/RoundTable.tsx
@@ -25,9 +25,9 @@ function RoundTable(
                         <td>{match.white}</td>
                         <td>
                             <select>
-                                <option selected={match.result == "WHITE_WIN"} value={"WHITE_WIN"}>White won</option>
-                                <option selected={match.result == "BLACK_WIN"} value={"BLACK_WIN"}>Black won</option>
-                                <option selected={match.result == "DRAW"} value={"DRAW"}>Draw</option>
+                                <option selected={match.result === "WHITE_WIN"} value={"WHITE_WIN"}>White won</option>
+                                <option selected={match.result === "BLACK_WIN"} value={"BLACK_WIN"}>Black won</option>
+                                <option selected={match.result === "DRAW"} value={"DRAW"}>Draw</option>
                             </select>
                         </td>
                         <td>{match.black}</td>
