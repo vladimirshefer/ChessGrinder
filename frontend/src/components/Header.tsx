9f2387d21064538471d4dc4304ab277682362709
Vladimir Shefer <quameu@gmail.com>
2023-08-30 23:13:11 +0400

    UI: Add styles to Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,10 +2,10 @@ import {Link} from "react-router-dom";
 
 function Header () {
     return <>
-        <div>
-            <Link to={"/"}><button>Home</button></Link>
-            Header Of Chess Grinder
-            <button>+ Tournament</button>
+        <div className={"w-full flex justify-between bg-blue-200 p-2"}>
+            <Link className={""} to={"/"}><button>Home</button></Link>
+            <h1 className={""}>Header Of Chess Grinder</h1>
+            <button className={"rounded-full bg-black text-blue-200 px-2"}>+ Tournament</button>
         </div>
     </>
 }
