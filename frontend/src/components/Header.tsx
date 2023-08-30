ac8bb1c407cb87eb30583574bab62e82aecc32e4
Vladimir Shefer <quameu@gmail.com>
2023-08-31 01:03:14 +0400

    UI: Remove +Tournament button from header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,7 +5,7 @@ function Header () {
         <div className={"w-full flex justify-between bg-blue-200 p-2"}>
             <Link className={""} to={"/"}><button>Home</button></Link>
             <h1 className={""}>Header Of Chess Grinder</h1>
-            <button className={"rounded-full bg-black text-blue-200 px-2"}>+ Tournament</button>
+            <div></div>
         </div>
     </>
 }
