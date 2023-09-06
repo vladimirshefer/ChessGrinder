2d18beeefa144ad1fde95e7e2120ebea87e310bb
Vladimir Shefer <quameu@gmail.com>
2023-09-07 03:54:10 +0400

    UI: FIX: rollback remove round tab route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,6 +19,7 @@ function App() {
               <Routes>
                 <Route path="/" element={<MainPage/>}/>
                 <Route path="/tournament/:id" element={<TournamentPage/>}/>
+                <Route path="/tournament/:id/round/:roundId" element={<TournamentPage/>}/>
                 <Route path="/admin" element={<AdminPage/>}/>
               </Routes>
             </React.StrictMode>
