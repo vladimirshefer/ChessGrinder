e6358ecbed3db5be9af7ebb6f856e23341d87f46
Vladimir Shefer <quameu@gmail.com>
2023-09-08 03:06:26 +0400

    UI: Redesign tournaments list

--- frontend/src/pages/MainPage/TournamentsList.tsx
+++ frontend/src/pages/MainPage/TournamentsList.tsx
@@ -13,7 +13,7 @@ export function TournamentsList(
     return <div>
         <h2 className={"text-xl my-2"}>Tournaments</h2>
         <ul className={"grid grid-cols-12"}>
-            <li className={"col-span-3 rounded-md text-blue-800 h-[20vh] bg-gray-100 m-2 py-2 flex justify-center content-center"}>
+            <li className={"col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 rounded-md text-blue-800 h-[20vh] bg-gray-100 m-2 py-2 flex justify-center content-center"}>
                 <div>
                     <button className={"h-full w-full"}
                             onClick={createTournament}
@@ -24,7 +24,7 @@ export function TournamentsList(
             </li>
             {tournaments.map(tournament => {
                 return <li key={tournament.id}
-                           className={"col-span-3 rounded-md h-[20vh] bg-gray-100 m-2 py-2 flex justify-center content-center"}>
+                           className={"col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 rounded-md h-[20vh] bg-gray-100 m-2 py-2 flex justify-center content-center"}>
                     <div>
                         <Link to={`/tournament/${tournament.id}`}>
                             {tournament.name || tournament.id}
