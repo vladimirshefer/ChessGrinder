5816d1e8859ced317a0ce1204d58e0a240fc6310
Vladimir Shefer <quameu@gmail.com>
2023-09-09 03:29:11 +0400

    UI: Header redesign

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,8 +3,7 @@ import {Link} from "react-router-dom";
 function Header () {
     return <>
         <div className={"w-full flex justify-between bg-blue-200 p-2"}>
-            <Link className={""} to={"/"}><button>Home</button></Link>
-            <h1 className={""}>Chess Grinder</h1>
+            <Link className={""} to={"/"}><h1 className={""}>Chess Grinder</h1></Link>
             <div>
                 <Link to={"/admin"}><button>Admin</button></Link>
             </div>
