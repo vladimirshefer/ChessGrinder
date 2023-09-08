ed06678eba1f057fca0101b76a646e685726cae5
Vladimir Shefer <quameu@gmail.com>
2023-09-09 03:27:00 +0400

    UI: TournamentPageRepository refactoring

--- frontend/src/lib/pageRepository/TournamentPageRepository.ts
+++ frontend/src/lib/pageRepository/TournamentPageRepository.ts
@@ -18,7 +18,10 @@ class LocalStorageTournamentPageRepository implements TournamentPageRepository {
     }
 
     async postParticipant(tournamentId: string, participant: string) {
-        let tournament = await this.getData(tournamentId) || this.getEmptyTournament(tournamentId);
+        let tournament = await this.getData(tournamentId);
+        if (!tournament) {
+            throw new Error(`No tournament with id ${tournamentId}`)
+        }
         tournament.participants = tournament.participants || []
         tournament.participants.push({
             name: participant,
