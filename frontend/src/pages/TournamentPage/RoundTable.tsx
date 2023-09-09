4116227af3c3e46031bfb0a46e324d86557177d0
Vladimir Shefer <quameu@gmail.com>
2023-09-09 13:36:53 +0400

    UI: Replace tebles with grids in round table

--- frontend/src/pages/TournamentPage/RoundTable.tsx
+++ frontend/src/pages/TournamentPage/RoundTable.tsx
@@ -1,4 +1,5 @@
 import {MatchDto, MatchResult} from "lib/api/dto/TournamentPageData";
+import {Fragment} from "react";
 
 function RoundTable(
     {
@@ -9,38 +10,30 @@ function RoundTable(
         submitMatchResult: (match: MatchDto, result: MatchResult | null) => void,
     }
 ) {
-    return <>
-        <table>
-            <thead>
-            <tr>
-                <th>White</th>
-                <th>Result</th>
-                <th>Black</th>
-            </tr>
-            </thead>
-            <tbody>
-            {
-                matches.map((match, idx) => {
-                    return <tr key={idx}>
-                        <td>{match.white.name}</td>
-                        <td>
-                            <select defaultValue={match.result || ""}
-                                    onChange={(e) => {
-                                        submitMatchResult(match, e.target.value as MatchResult)
-                                    }}>
-                                <option value={""}>Unknown</option>
-                                <option value={"WHITE_WIN"}>White won</option>
-                                <option value={"BLACK_WIN"}>Black won</option>
-                                <option value={"DRAW"}>Draw</option>
-                            </select>
-                        </td>
-                        <td>{match.black.name}</td>
-                    </tr>
-                })
-            }
-            </tbody>
-        </table>
-    </>
+    return <div className={"grid grid-cols-12"}>
+        <div className={"col-span-4 font-bold"}>White</div>
+        <div className={"col-span-4 font-bold"}>Result</div>
+        <div className={"col-span-4 font-bold"}>Black</div>
+        {
+            matches.map((match, idx) => {
+                return <Fragment key={idx}>
+                    <div className={"col-span-4"}>{match.white.name}</div>
+                    <div className={"col-span-4"}>
+                        <select defaultValue={match.result || ""}
+                                onChange={(e) => {
+                                    submitMatchResult(match, e.target.value as MatchResult)
+                                }}>
+                            <option value={""}>Unknown</option>
+                            <option value={"WHITE_WIN"}>White won</option>
+                            <option value={"BLACK_WIN"}>Black won</option>
+                            <option value={"DRAW"}>Draw</option>
+                        </select>
+                    </div>
+                    <div className={"col-span-4"}>{match.black.name}</div>
+                </Fragment>
+            })
+        }
+    </div>
 }
 
 export default RoundTable
