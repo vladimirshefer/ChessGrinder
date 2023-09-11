e8705d6b2d0c62b62d00c2319ac703ac93578f59
Vladimir Shefer <quameu@gmail.com>
2023-09-11 04:14:02 +0400

    UI: Temporarily switch to hash router for github pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, HashRouter, Route, Routes} from "react-router-dom";
 import MainPage from "pages/MainPage"
 import TournamentPage from "pages/TournamentPage"
 import Header from "components/Header";
@@ -10,11 +10,13 @@ import LoginPage from "pages/LoginPage";
 
 const queryClient = new QueryClient()
 
+let ApplicationRouter = 1 > 0 /*TODO*/ ? HashRouter : BrowserRouter
+
 function App() {
   return (
       <div className='App'>
         <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
+          <ApplicationRouter>
             <React.StrictMode>
               <Header/>
               <Routes>
@@ -25,7 +27,7 @@ function App() {
                 <Route path="/admin" element={<AdminPage/>}/>
               </Routes>
             </React.StrictMode>
-          </BrowserRouter>
+          </ApplicationRouter>
         </QueryClientProvider>
       </div>
   );
