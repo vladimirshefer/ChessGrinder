8a2792bcd9a1b9549a5ce816bf7e47d0d29ddf39
Vladimir Shefer <quameu@gmail.com>
2023-09-17 18:03:20 +0400

    REST: Add User login fields

--- src/main/java/com/chessgrinder/chessgrinder/entities/User.java
+++ src/main/java/com/chessgrinder/chessgrinder/entities/User.java
@@ -2,6 +2,7 @@ package com.chessgrinder.chessgrinder.entities;
 
 import java.util.*;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.extern.slf4j.*;
@@ -16,11 +17,28 @@ import lombok.extern.slf4j.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-
     @Id
-    @Column(name = "id")
+    @Column(name = "id", nullable = false)
     private UUID id;
 
-    @Column(name = "name")
+    /**
+     * User login. Unique across all users. E.g. "vshefer".
+     * Could be null for "guest" users.
+     */
+    @Column(name = "username")
+    @Nullable
+    private String username;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    /**
+     * Hash of the user.
+     */
+    @Column(name = "password")
+    @Nullable
+    private String password;
+
+    @Column(name = "is_admin", nullable = false)
+    private boolean isAdmin = false;
 }
