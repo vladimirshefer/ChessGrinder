7f19d350e57f9db2209ac12d982fd4abdabbda4c
Alexander Boldyrev <a.boldyrev@fundcount.com>
2023-09-17 17:45:00 +0400

    REST: Time issue

--- src/main/java/com/chessgrinder/chessgrinder/dto/TournamentDto.java
+++ src/main/java/com/chessgrinder/chessgrinder/dto/TournamentDto.java
@@ -4,6 +4,7 @@ import java.time.*;
 import java.util.*;
 
 import com.chessgrinder.chessgrinder.enums.*;
+import com.fasterxml.jackson.annotation.*;
 import lombok.*;
 
 @Data
@@ -11,7 +12,10 @@ import lombok.*;
 public class TournamentDto {
 
     private String id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDateTime date;
+
     private TournamentStatus status;
 
 
