cf3ed9ca84bde9ece5de66c0489723405f0bde23
Vladimir Shefer <quameu@gmail.com>
2023-09-17 18:02:52 +0400

    REST: MemberDto javadocs

--- src/main/java/com/chessgrinder/chessgrinder/dto/MemberDto.java
+++ src/main/java/com/chessgrinder/chessgrinder/dto/MemberDto.java
@@ -7,7 +7,22 @@ import lombok.*;
 @Data
 @Builder
 public class MemberDto {
+    /**
+     * UUID string. Unique across all users.
+     */
     private String id;
+    /**
+     * User login. Unique across all users. E.g. "vshefer".
+     * Could be null for "guest" users.
+     */
+    private String username;
+    /**
+     * User full name. E.g. "Vladimir Shefer"
+     */
     private String name;
+
+    /**
+     * List of Badges (i.e. Achievements) of the user.
+     */
     private List<BadgeDto> badges;
 }
