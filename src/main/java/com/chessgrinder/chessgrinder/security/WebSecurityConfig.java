2390b936de186b58a0db14a1d0bc3c8d682f91ba
Vladimir Shefer <quameu@gmail.com>
2023-10-01 18:51:29 +0400

    REST: Fix compilation error

--- src/main/java/com/chessgrinder/chessgrinder/security/WebSecurityConfig.java
+++ src/main/java/com/chessgrinder/chessgrinder/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.chessgrinder.chessgrinder.security;
 
 import com.chessgrinder.chessgrinder.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
