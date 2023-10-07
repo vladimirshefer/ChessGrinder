3c8b8e7d56c3d5722dff5d868d59c80a1f786adf
Vladimir Shefer <quameu@gmail.com>
2023-10-07 16:58:02 +0400

    UI: TournamentsList redesign

--- frontend/src/pages/MainPage/TournamentsList.tsx
+++ frontend/src/pages/MainPage/TournamentsList.tsx
@@ -1,10 +1,86 @@
 import {Link} from "react-router-dom";
 import {TournamentDto, UserRoles} from "lib/api/dto/MainPageData";
-import ConditionalOnUserRole, {ConditionalOnAuthorized} from "components/Conditional";
+import ConditionalOnUserRole, {Conditional, ConditionalOnAuthorized} from "components/Conditional";
 import loc from "strings/loc";
 import tournamentRepository from "lib/api/repository/TournamentRepository";
 import {AiFillClockCircle} from "react-icons/ai";
 import {IoLocationSharp} from "react-icons/io5";
+import "./TournamentsList.css"
+
+function TournamentPane(
+    {
+        tournament
+    }: {
+        tournament: TournamentDto
+    }
+) {
+    let isPlanned = tournament.status === "PLANNED"
+    let isFinished = tournament.status === "FINISHED"
+    let isActive = tournament.status === "ACTIVE"
+
+    return <div className={`grid justify-items-start w-full p-4 
+                ${isPlanned ? "tournament-planned" : isFinished ? "tournament-finished" : "tournament-active"}`}>
+        <div className={"grid justify-items-start"}>
+            <Link className={"font-bold text-lg text-left"} to={`/tournament/${tournament.id}`}>
+                {tournament.name || tournament.id}
+            </Link>
+            <small className={"font-bold text-left"}>
+                {tournament.date}
+            </small>
+        </div>
+        <div className={"p-1"}></div>
+        <Conditional on={isPlanned || isActive}>
+            <div className={"py-3"}>
+                            <span className={"flex items-center"}>
+                                <AiFillClockCircle className={"text-primary mr-3"}/>
+                                20:00
+                            </span>
+                <span className={"flex items-center"}>
+                                <IoLocationSharp className={"text-primary mr-3"}/>
+                                Seven roads
+                            </span>
+            </div>
+        </Conditional>
+        <Conditional on={isFinished}>
+            <div className={"py-3 flex gap-3 uppercase text-left"}>
+                <div className={"grid justify-items-start content-start"}>
+                    <span className={"text-sm"}>Победитель</span>
+                    <span className={"font-bold"}>Александр Болдырев</span>
+                </div>
+                <div className={"grid justify-items-start content-start"}>
+                    <span className={"text-sm"}>Результат</span>
+                    <span className={"font-bold"}>7</span>
+                </div>
+            </div>
+        </Conditional>
+        <div className={"p-1"}></div>
+        <Conditional on={isPlanned || isActive}>
+            <div className={"w-full"}>
+                <ConditionalOnAuthorized>
+                    <button className={"btn-primary w-full"}
+                            onClick={() => tournamentRepository.participate(tournament.id)}
+                    >
+                        {loc("Participate")}
+                    </button>
+                </ConditionalOnAuthorized>
+                <ConditionalOnAuthorized authorized={false}>
+                    <Link to={"/login"} className={"w-full"}>
+                        <button className={"btn-primary w-full"}>
+                            {loc("Participate")}
+                        </button>
+                    </Link>
+                </ConditionalOnAuthorized>
+            </div>
+        </Conditional>
+        <Conditional on={isFinished}>
+            <button className={"btn-dark w-full"}
+                    onClick={() => tournamentRepository.participate(tournament.id)}
+            >
+                {loc("More info")}
+            </button>
+        </Conditional>
+    </div>;
+}
 
 export function TournamentsList(
     {
@@ -18,58 +94,22 @@ export function TournamentsList(
 
     return <div>
         <h2 className={"text-xl my-2"}>{loc("Tournaments")}</h2>
-        <ul className={"grid grid-cols-12"}>
+        <ul className={"grid grid-cols-12 gap-2"}>
             <ConditionalOnUserRole role={UserRoles.ADMIN}>
-                <li className={"col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 rounded-md text-blue-800 h-[20vh] bg-gray-100 m-2 py-2 flex justify-center content-center"}>
-                    <div>
-                        <button className={"h-full w-full"}
-                                onClick={createTournament}
-                        >
-                            + {loc("Tournament")}
-                        </button>
-                    </div>
+                <li className={"col-span-12 md:col-span-6 lg:col-span-4 xl:col-span-3 " +
+                    "bg-gray-200 flex justify-center content-center"}>
+                    <button className={"h-full w-full text-xl text-primary "}
+                            onClick={createTournament}
+                    >
+                        + {loc("Tournament")}
+                    </button>
                 </li>
             </ConditionalOnUserRole>
             {tournaments.map(tournament => {
                 return <li key={tournament.id}
-                           className={`col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 bg-black text-white 
-                                        m-2 flex content-center`}>
-                    <div className={"grig grid-cols-12 w-full p-4"}>
-                        <div className={"col-span-12 grid justify-items-start"}>
-                            <Link className={"font-bold text-lg text-left"} to={`/tournament/${tournament.id}`}>
-                                {tournament.name || tournament.id}
-                            </Link>
-                            <small className={"font-bold text-left"}>
-                                {tournament.date}
-                            </small>
-                        </div>
-                        <div className={"py-3"}>
-                            <span className={"flex items-center"}>
-                                <AiFillClockCircle className={"text-primary mr-3"}/>
-                                20:00
-                            </span>
-                            <span className={"flex items-center"}>
-                                <IoLocationSharp className={"text-primary mr-3"}/>
-                                Seven roads
-                            </span>
-                        </div>
-                        <div className={"col-span-12"}>
-                            <ConditionalOnAuthorized>
-                                <button className={"btn-primary w-full"}
-                                        onClick={() => tournamentRepository.participate(tournament.id)}
-                                >
-                                    {loc("Participate")}
-                                </button>
-                            </ConditionalOnAuthorized>
-                            <ConditionalOnAuthorized authorized={false}>
-                                <Link to={"/login"}>
-                                    <button className={"btn-primary w-full"}>
-                                        {loc("Participate")}
-                                    </button>
-                                </Link>
-                            </ConditionalOnAuthorized>
-                        </div>
-                    </div>
+                           className={`col-span-12 md:col-span-6 lg:col-span-4 xl:col-span-3 
+                                       flex content-center`}>
+                    <TournamentPane tournament={tournament}/>
                 </li>
             })}
         </ul>
