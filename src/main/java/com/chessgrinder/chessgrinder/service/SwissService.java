9f860df0c4be677e29a096292fbec442b999c862
Alexander Boldyrev <a.boldyrev@fundcount.com>
2023-10-08 14:18:36 +0400

    Backend: Swiss draw

--- src/main/java/com/chessgrinder/chessgrinder/service/SwissService.java
+++ src/main/java/com/chessgrinder/chessgrinder/service/SwissService.java
@@ -2,8 +2,7 @@ package com.chessgrinder.chessgrinder.service;
 
 import com.chessgrinder.chessgrinder.dto.*;
 import com.chessgrinder.chessgrinder.dto.internals.*;
-import com.chessgrinder.chessgrinder.entities.MatchEntity;
-import com.chessgrinder.chessgrinder.repositories.MatchRepository;
+import com.chessgrinder.chessgrinder.enums.*;
 import com.google.common.collect.Lists;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,24 +10,23 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.*;
+import java.util.stream.*;
 
 @Component
 @RequiredArgsConstructor
 @Getter
 public class SwissService {
 
-    private final MatchRepository matchRepository;
-    private String tournamentId;
-
+    /**
+     * "Main" method for swiss service. Takes list of all participants and return list of all matches.
+     * @param participants
+     * @return
+     */
     public List<MatchDto> makePairs(List<ParticipantDto> participants) {
 
-        List<MatchEntity> matches = new ArrayList<>();
+        List<MatchDto> matches = new ArrayList<>();
 
         if (participants.isEmpty()) return Collections.emptyList();
-
-/*
-        tournamentId = participants.get(0).getTournament().getId();
-*/
 
         List<ParticipantDto> sortedParticipants = participants.stream()
                 .sorted(Comparator.comparing(ParticipantDto::getScore).reversed()).toList();
@@ -49,84 +47,182 @@ public class SwissService {
                 continue;
             }
 
-            firstCandidateForMatch.setBooked(true);
-            ParticipantDto secondCandidateForMatch = findMatchForParticipant(listOfAllPlayers, firstCandidateForMatch);
+            ParticipantForPairing secondCandidateForMatch = findPairForParticipant(listOfAllPlayers, firstCandidateForMatch);
 
+            MatchDto match = createMatchBetweenTwoParticipants(firstCandidateForMatch, secondCandidateForMatch);
+            matches.add(match);
         }
 
-        return null;
+        return matches;
     }
-    public ParticipantDto findMatchForParticipant(List<ParticipantForPairing> players, ParticipantForPairing firstCandidateForMatch) {
-        List<ScoreModel> separateParticipantsByScores = separateParticipantsByScores(players);
+
+    /**
+     *  Returns secondCandidateForMatch. Finds the closest score gap and selects first player from the second half of group.
+     * @param players all participants in the tournament
+     * @param firstCandidateForMatch first candidate for match.
+     * @return
+     */
+    public ParticipantForPairing findPairForParticipant(List<ParticipantForPairing> players, ParticipantForPairing firstCandidateForMatch) {
+
+        List<ScoreModel> separateParticipantsByScores = sortParticipantsByScores(players);
         List<ScoreModel> scoreModels = makeEvenValues(separateParticipantsByScores);
 
-        return null;
+        ScoreModel scoreModelWithClosestScore = findScoreModelWithClosestScore(scoreModels, firstCandidateForMatch);
+
+        List<ParticipantForPairing> participants = scoreModelWithClosestScore.getParticipants();
+        var split = split(participants);
+
+        if (split.length == 0) {
+            return null;
+        }
+
+        return (ParticipantForPairing) split[1].get(0);
+    }
+
+
+    /**
+     *  SecondCandidateForMatch should have the closest score to the firstCandidateForMatch. This method returns ScoreModel
+     *  which represents list of possible candidates.
+     * @param scoreModels
+     * @param firstCandidateForMatch
+     * @return
+     */
+    public ScoreModel findScoreModelWithClosestScore(List<ScoreModel> scoreModels, ParticipantForPairing firstCandidateForMatch) {
+        if (scoreModels.isEmpty()) {
+            return null;
+        }
+
+        BigDecimal targetScore = firstCandidateForMatch.getParticipant().getScore();
+        ScoreModel closestScoreModel = scoreModels.get(0); // Initialize with the first ScoreModel
+
+        BigDecimal closestDifference = targetScore.subtract(closestScoreModel.getScore()).abs();
+
+        for (ScoreModel scoreModel : scoreModels) {
+            BigDecimal difference = targetScore.subtract(scoreModel.getScore()).abs();
+
+            if (difference.compareTo(closestDifference) < 0) {
+                // If the current ScoreModel has a closer score
+                closestDifference = difference;
+                closestScoreModel = scoreModel;
+            }
+        }
+
+        return closestScoreModel;
     }
 
 
     public static<T> List[] split(List<T> list) {
-        if (list.isEmpty()) {
+        if (list.isEmpty() || list.size() == 1) {
             return new List[]{};
         }
         List<List<T>> lists = Lists.partition(list, (list.size() + 1) / 2);
         return new List[] {lists.get(0), lists.get(1)};
     }
 
-    public List<ScoreModel> separateParticipantsByScores (List<ParticipantForPairing> participants) {
-
-        Map<BigDecimal, List<ParticipantForPairing>> map = new TreeMap<>(Collections.reverseOrder());
-        List<ScoreModel> resultList = new ArrayList<>();
-
-        for (ParticipantForPairing participant: participants) {
-            BigDecimal score = participant.getParticipant().getScore();
-
-            List<ParticipantForPairing> participantsWithSameScore = map.getOrDefault(score, new ArrayList<>());
-
-            participantsWithSameScore.add(participant);
-
-            ScoreModel scoreModel = ScoreModel.builder().score(score).participants(participantsWithSameScore).build();
-            resultList.add(scoreModel);
-
-        }
-
-        return resultList;
-    }
 
     public List<ScoreModel> makeEvenValues(List<ScoreModel> scoreModels) {
 
-        int currentIndex = 0;
-        List<ScoreModel> modelsToRemove = new ArrayList<>();
+        int lastIndex = scoreModels.size() - 1;
 
-        while (currentIndex < scoreModels.size()) {
-            ScoreModel currentScoreModel = scoreModels.get(currentIndex);
+        for (int i = 0; i < scoreModels.size() - 1; i++) {
+            ScoreModel currentModel = scoreModels.get(i);
+            ScoreModel nextModel = scoreModels.get(i + 1);
 
-            if (currentScoreModel.getParticipants().size() % 2 != 0) {
-                // Current ScoreModel has an odd number of participants
-                if (currentIndex < scoreModels.size() - 1) {
-                    // There is a next ScoreModel available
-                    ScoreModel nextScoreModel = scoreModels.get(currentIndex + 1);
-                    ParticipantForPairing firstParticipant = nextScoreModel.getParticipants().get(0);
-                    currentScoreModel.getParticipants().add(firstParticipant);
-                    nextScoreModel.getParticipants().remove(0);
-                } else {
-                    // There is no next ScoreModel, so we cannot fix it
-                    break;
-                }
-            } else {
-                // Current ScoreModel has an even number of participants, no action needed
-                currentIndex++;
-            }
-
-            if (currentScoreModel.getParticipants().isEmpty()) {
-                // Mark the current ScoreModel for removal
-                modelsToRemove.add(currentScoreModel);
+            if (currentModel.getParticipants().size() % 2 != 0) {
+                ParticipantForPairing participantToMove = nextModel.getParticipants().get(0);
+                currentModel.getParticipants().add(participantToMove);
+                nextModel.getParticipants().remove(0);
             }
         }
-        scoreModels.removeAll(modelsToRemove);
+
+        ScoreModel lastModel = scoreModels.get(lastIndex);
+        if (lastModel.getParticipants().size() % 2 != 0) {
+            ScoreModel newModel = ScoreModel.builder()
+                    .score(BigDecimal.valueOf(-1))
+                    .participants(new ArrayList<>())
+                    .build();
+            newModel.getParticipants().add(lastModel.getParticipants().get(lastModel.getParticipants().size() - 1));
+            lastModel.getParticipants().remove(lastModel.getParticipants().size() - 1);
+            scoreModels.add(newModel);
+        }
+
+        scoreModels.removeIf(model -> model.getParticipants().isEmpty());
+
         return scoreModels;
     }
 
+    /**
+     *  Creates MatchDto object for two participants in the tournament.
+     *  The player who has played fewer games with the white pieces in the tournament will now be playing as white.
+     *
+     * @param first player
+     * @param second player
+     * @return MatchDto
+     */
+    public MatchDto createMatchBetweenTwoParticipants(ParticipantForPairing first, ParticipantForPairing second) {
 
+        MatchDto match;
+
+        if (second == null) {
+
+            return MatchDto.builder()
+                    .white(first.getParticipant())
+                    .result(MatchResult.BUY)
+                    .build();
+        }
+
+        if (first.getTimesPlayedWhiteInTournament() <= second.getTimesPlayedWhiteInTournament()) {
+            match = MatchDto.builder()
+                    .white(first.getParticipant())
+                    .black(second.getParticipant())
+                    .build();
+        } else {
+            match = MatchDto.builder()
+                    .white(second.getParticipant())
+                    .black(first.getParticipant())
+                    .build();
+        }
+
+        first.setBooked(true);
+        second.setBooked(true);
+
+        return match;
+    }
+
+    /**
+     *  Returns list of special ScoreModel objects which groups and sorts all participants by scores.
+     *
+     * @param participants All players in the tournament.
+     * @return List<ScoreModel>
+     */
+    public static List<ScoreModel> sortParticipantsByScores(List<ParticipantForPairing> participants) {
+
+        // Filter participants who have not booked.
+        List<ParticipantForPairing> notBookedParticipants = participants.stream()
+                .filter(player -> !player.isBooked()).toList();
+
+        // Group participants by their scores.
+        Map<BigDecimal, List<ParticipantForPairing>> groupedParticipants = notBookedParticipants.stream()
+                .collect(Collectors.groupingBy(
+                        participant -> participant.getParticipant().getScore(),
+                        Collectors.toList()
+                ));
+
+        // Convert the grouped participants into ScoreModel objects.
+        List<ScoreModel> scoreModels = new ArrayList<>();
+        for (Map.Entry<BigDecimal, List<ParticipantForPairing>> entry : groupedParticipants.entrySet()) {
+            ScoreModel scoreModel = ScoreModel.builder()
+                    .score(entry.getKey())
+                    .participants(entry.getValue())
+                    .build();
+            scoreModels.add(scoreModel);
+        }
+
+        // Sorting by score.
+        scoreModels.sort((a, b) -> b.getScore().compareTo(a.getScore()));
+
+        return scoreModels;
+    }
 
 }
 
