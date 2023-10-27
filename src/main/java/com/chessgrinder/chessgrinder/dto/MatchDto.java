8862b4002124a55fbcf1c90843d0dfc44d1610ca
Vladimir Shefer <quameu@gmail.com>
2023-10-27 22:02:34 +0400

    REST: Nullability annotations

--- src/main/java/com/chessgrinder/chessgrinder/dto/MatchDto.java
+++ src/main/java/com/chessgrinder/chessgrinder/dto/MatchDto.java
@@ -1,6 +1,7 @@
 package com.chessgrinder.chessgrinder.dto;
 
 import com.chessgrinder.chessgrinder.enums.*;
+import jakarta.annotation.Nullable;
 import lombok.*;
 
 @Data
@@ -9,5 +10,6 @@ public class MatchDto {
     private String id;
     private ParticipantDto white;
     private ParticipantDto black;
+    @Nullable
     private MatchResult result;
 }
