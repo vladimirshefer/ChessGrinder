17551a21a2d13167e13c276a497d02ffb12abbd8
Vladimir Shefer <quameu@gmail.com>
2023-11-11 02:12:21 +0400

    UI: Change participant nickname

--- frontend/src/pages/ParticipantPage/index.tsx
+++ frontend/src/pages/ParticipantPage/index.tsx
@@ -4,9 +4,12 @@ import {useQuery} from "@tanstack/react-query";
 import userRepository from "lib/api/repository/UserRepository";
 import ConditionalOnUserRole, {Conditional} from "components/Conditional";
 import {UserRoles} from "lib/api/dto/MainPageData";
+import {useForm} from "react-hook-form";
+import {propagate} from "lib/util/misc";
 
 export default function ParticipantPage() {
     let navigate = useNavigate()
+    let editForm = useForm()
 
     let {
         tournamentId,
@@ -52,6 +55,19 @@ export default function ParticipantPage() {
         </>
     }
 
+    async function changeNickname(data: { [key: string]: string}) {
+        await participantRepository.updateParticipant(tournamentId!!, {
+            id: participantQuery.data?.id || "",
+            name: data["nickname"],
+            userId: "",
+            score: 0,
+            userFullName: "",
+            buchholz: 0,
+        })
+            .catch(propagate(() => "Could not change nickname!"))
+        await participantQuery.refetch()
+    }
+
     return <div className={"p-3"}>
         <div className={"p-3"}>
             <Conditional on={participantQuery.isSuccess}>
@@ -66,6 +82,24 @@ export default function ParticipantPage() {
         </div>
 
         <div></div>
+
+        <ConditionalOnUserRole role={UserRoles.ADMIN}>
+            <form onSubmit={editForm.handleSubmit(changeNickname)} className={"grid p-2 gap-2 border my-2"}>
+                <h3 className={"uppercase text-start"}>
+                    {"Change nickname"}
+                </h3>
+                <div className={"flex gap-2"}>
+                    <input
+                        className={"input-text grow"}
+                        type={"text"}
+                        defaultValue={participantQuery.data?.name || ""}
+                        placeholder={"Nickname"}
+                        {...editForm.register("nickname")}
+                    />
+                    <button className={"btn-light"} type={"submit"}>Save</button>
+                </div>
+            </form>
+        </ConditionalOnUserRole>
 
         <div className={"flex gap-2 justify-end"}>
             <Link to={`/tournament/${tournamentId}`}>
