ba26b168cd61eb7275b14a2f114db26705831e54
Umar Shabazov <malik95i@yandex.ru>
2024-02-11 15:10:53 +0300

    Fixes after code review for "Do not finish round while game is on"

--- lib/install.sh
+++ lib/install.sh
@@ -1,2 +1,2 @@
 # Install maven dependencies to local repository so that they could be used in the pom.xml
-./mvnw install:install-file -Dfile=./lib/javafo-2.2-main.jar -DgroupId=javafo -DartifactId=javafo -Dversion=2.2 -Dpackaging=jar
+mvn install:install-file -Dfile=./javafo-2.2-main.jar -DgroupId=javafo -DartifactId=javafo -Dversion=2.2 -Dpackaging=jar
