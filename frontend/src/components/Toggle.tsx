963d3f934d27ce38926e54954e2ba465190c2670
Vladimir Shefer <quameu@gmail.com>
2024-02-17 02:33:43 +0400

    UI: ParticipantPage: Mising toogle, accessibility

---
+++ frontend/src/components/Toggle.tsx
@@ -0,0 +1,59 @@
+import React, {useState} from "react";
+
+export default function Toggle(
+    {
+        checked,
+        setChecked,
+        defaultChecked,
+        title,
+    }: {
+        checked?: boolean | undefined,
+        setChecked?: ((c: boolean) => void) | undefined,
+        defaultChecked?: boolean | undefined,
+        title?: string | undefined
+    }
+) {
+    let [checkedInternal, setCheckedInternal] = useState(defaultChecked || false)
+
+    let checkedReal = checked !== undefined ? checked : checkedInternal
+
+    // Source: https://flowbite.com/docs/forms/toggle/
+    return <label className="inline-flex items-center cursor-pointer" title={title}>
+        <input type="checkbox" className="sr-only peer" checked={checkedReal} title={title}
+               onChange={e => {
+                   setChecked === undefined || setChecked(e.target.checked)
+                   setCheckedInternal(e.target.checked)
+               }}
+        />
+        <div className="relative
+                        w-11
+                        h-6
+                        bg-gray-200
+                        peer-focus:outline-none
+                        peer-focus:ring-4
+                        peer-focus:ring-anzac-300
+                        dark:peer-focus:ring-anzac-800
+                        rounded-full
+                        peer
+                        dark:bg-gray-700
+                        peer-checked:after:translate-x-full
+                        rtl:peer-checked:after:-translate-x-full
+                        peer-checked:after:border-white
+                        after:content-['']
+                        after:absolute
+                        after:top-[2px]
+                        after:start-[2px]
+                        after:bg-white
+                        after:border-gray-300
+                        after:border
+                        after:rounded-full
+                        after:h-5
+                        after:w-5
+                        after:transition-all
+                        dark:border-gray-600
+                        peer-checked:bg-anzac-600
+                        "
+             title={title}
+        ></div>
+    </label>;
+}
