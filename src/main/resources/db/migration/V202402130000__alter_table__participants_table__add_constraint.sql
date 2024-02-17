b26aa8bcb7a7d3a71b7de7b92c4268944d2ca137
Семен Александрович Дубенков <61518799+sedub01@users.noreply.github.com>
2024-02-17 09:46:41 +0300

    fix: delete participants after tournament (#12)
    * feat: add SQL migrations
    
    * fix: resolve review problems
    
    ---------
    
    Co-authored-by: sedub01

---
+++ src/main/resources/db/migration/V202402130000__alter_table__participants_table__add_constraint.sql
@@ -0,0 +1,11 @@
+DELETE FROM participants_table WHERE tournament_id IS NULL;
+
+ALTER TABLE participants_table
+    DROP CONSTRAINT fk__participants_table__tournaments_table;
+
+ALTER TABLE participants_table
+    ADD CONSTRAINT fk__participants_table__tournaments_table
+        FOREIGN KEY (tournament_id) REFERENCES tournaments_table
+            ON DELETE CASCADE,
+    ADD CONSTRAINT unique_id_tournament_id UNIQUE (id, tournament_id),
+    ALTER COLUMN tournament_id SET NOT NULL;
