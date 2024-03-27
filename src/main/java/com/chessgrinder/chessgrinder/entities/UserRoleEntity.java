450bf1f8ba095c20dcd33125cdbf0cc409964a3d
sedub01 <s.dubenkov@gk-adept.ru>
2024-03-27 17:45:01 +0300

    feat: add auditing for users_roles_table

--- src/main/java/com/chessgrinder/chessgrinder/entities/UserRoleEntity.java
+++ src/main/java/com/chessgrinder/chessgrinder/entities/UserRoleEntity.java
@@ -4,7 +4,13 @@ import jakarta.persistence.*;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.Instant;
 import java.util.UUID;
 
 @Getter
@@ -16,6 +22,7 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class UserRoleEntity {
 
     @Id
@@ -31,4 +38,19 @@ public class UserRoleEntity {
     @JoinColumn(name="role_id", nullable = false)
     private RoleEntity role;
 
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private Instant createdAt;
+
+    @Column(name = "updated_at", insertable = false)
+    @LastModifiedDate
+    private Instant updatedAt;
+
+    @Column(name = "created_by", nullable = false, updatable = false)
+    @CreatedBy
+    private String createdBy;
+
+    @Column(name = "updated_by", insertable = false)
+    @LastModifiedBy
+    private String updatedBy;
 }
--- src/main/resources/db/migration/V202403130000__alter_tables__add_auditing_columns.sql
+++ src/main/resources/db/migration/V202403130000__alter_tables__add_auditing_columns.sql
@@ -5,3 +5,9 @@ ALTER TABLE users_table
     ADD COLUMN updated_by VARCHAR(255);
 UPDATE users_table SET created_by = username;
 ALTER TABLE users_table ALTER COLUMN created_by SET NOT NULL;
+
+ALTER TABLE users_roles_table
+    ADD COLUMN created_at TIMESTAMP DEFAULT NOW(),
+    ADD COLUMN updated_at TIMESTAMP,
+    ADD COLUMN created_by VARCHAR(255),
+    ADD COLUMN updated_by VARCHAR(255);
