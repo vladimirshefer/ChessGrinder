c192297116127840a246065f0af107dbfd9ec916
Vladimir Shefer <quameu@gmail.com>
2024-06-23 22:07:32 +0200

    REST: Spring Security: Permission security

--- src/main/java/com/chessgrinder/chessgrinder/security/SecurityUtil.java
+++ src/main/java/com/chessgrinder/chessgrinder/security/SecurityUtil.java
@@ -1,6 +1,8 @@
 package com.chessgrinder.chessgrinder.security;
 
 import com.chessgrinder.chessgrinder.entities.UserEntity;
+import jakarta.annotation.Nullable;
+import org.springframework.security.core.Authentication;
 
 public final class SecurityUtil {
 
@@ -8,6 +10,37 @@ public final class SecurityUtil {
         return user.getRoles().stream().anyMatch(it -> it.getName().equalsIgnoreCase(role));
     }
 
+    @Nullable
+    public static UserEntity tryGetUserEntity(@Nullable Authentication authentication) {
+        if (authentication == null) {
+            return null;
+        }
+
+        if (authentication instanceof UserEntity userEntity) {
+            return userEntity;
+        }
+
+        if (authentication instanceof AuthorizedUserEntityProvider authorizedUserEntityProvider) {
+            UserEntity userEntity = authorizedUserEntityProvider.getUserEntity();
+            if (userEntity != null) {
+                return userEntity;
+            }
+        }
+
+        if (authentication.getPrincipal() instanceof UserEntity userEntity) {
+            return userEntity;
+        }
+
+        if (authentication.getPrincipal() instanceof AuthorizedUserEntityProvider authorizedUserEntityProvider) {
+            UserEntity userEntity = authorizedUserEntityProvider.getUserEntity();
+            if (userEntity != null) {
+                return userEntity;
+            }
+        }
+        return null;
+    }
+
     private SecurityUtil() {
     }
+
 }
