625047bcc936fc49a062ed5e3dee2b7e3e4ea025
Vladimir Shefer <quameu@gmail.com>
2024-06-28 22:54:40 +0200

    REST: Refactoring in CheatController

--- src/main/java/com/chessgrinder/chessgrinder/controller/CheatController.java
+++ src/main/java/com/chessgrinder/chessgrinder/controller/CheatController.java
@@ -4,11 +4,12 @@ import com.chessgrinder.chessgrinder.entities.RoleEntity;
 import com.chessgrinder.chessgrinder.entities.UserEntity;
 import com.chessgrinder.chessgrinder.entities.UserRoleEntity;
 import com.chessgrinder.chessgrinder.repositories.RoleRepository;
-import com.chessgrinder.chessgrinder.repositories.UserRepository;
 import com.chessgrinder.chessgrinder.repositories.UserRoleRepository;
+import com.chessgrinder.chessgrinder.security.AuthenticatedUserArgumentResolver.AuthenticatedUser;
+import com.chessgrinder.chessgrinder.security.SecurityUtil;
+import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,33 +27,31 @@ public class CheatController {
 
     private final RoleRepository roleRepository;
     private final UserRoleRepository userRoleRepository;
-    private final UserRepository userRepository;
 
     @GetMapping("/getAdminRole")
     public Object getAdminRole(
-            Authentication authentication
+            @AuthenticatedUser(required = false)
+            @Nullable
+            UserEntity user
     ) {
-        if (authentication == null || !authentication.isAuthenticated()) {
+        if (user == null) {
             throw new ResponseStatusException(401, "Please log in", null);
         }
-        if (!authentication.getAuthorities().stream().anyMatch(it -> it.getAuthority().equals(RoleEntity.Roles.ADMIN))) {
-            UserEntity user = userRepository.findByUsername(authentication.getName());
-            if (user == null) {
-                throw new ResponseStatusException(401, "User does not exist", null);
-            }
-            if (user.getRoles().stream().anyMatch(it -> it.getName().equals(RoleEntity.Roles.ADMIN))) {
-                return "User already has admin role";
-            }
 
-            RoleEntity adminRole = roleRepository.findByName(RoleEntity.Roles.ADMIN)
-                    .orElseGet(() -> roleRepository.save(RoleEntity.builder().name(RoleEntity.Roles.ADMIN).build()));
-            userRoleRepository.save(UserRoleEntity.builder()
-                            .user(user)
-                            .role(adminRole)
-                    .build());
-            return "Admin role granted";
+        if (SecurityUtil.hasRole(user, RoleEntity.Roles.ADMIN)) {
+            return "User already has ADMIN authority";
         }
-        return "User already has ADMIN authority";
+
+        RoleEntity adminRole = roleRepository.findByName(RoleEntity.Roles.ADMIN)
+                .orElseGet(() -> roleRepository.save(RoleEntity.builder().name(RoleEntity.Roles.ADMIN).build()));
+
+        userRoleRepository.save(UserRoleEntity.builder()
+                .user(user)
+                .role(adminRole)
+                .build()
+        );
+
+        return "Admin role granted";
     }
 
 }
