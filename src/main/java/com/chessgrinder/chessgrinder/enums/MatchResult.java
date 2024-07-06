23fcf0c08d9b7318a0eaa11ac613474106782a54
Vladimir Shefer <quameu@gmail.com>
2024-07-06 10:14:31 +0200

    REST: Minor: Comment in MatchResult

--- src/main/java/com/chessgrinder/chessgrinder/enums/MatchResult.java
+++ src/main/java/com/chessgrinder/chessgrinder/enums/MatchResult.java
@@ -1,8 +1,13 @@
 package com.chessgrinder.chessgrinder.enums;
 
 public enum MatchResult {
-    WHITE_WIN, BLACK_WIN, DRAW, BUY,
+    WHITE_WIN, BLACK_WIN, DRAW,
     /**
+     * Equals to full point bye
+     */
+    BUY,
+    /**
+     * Equals to zero point bye.
      * If player is missing or disqualified
      */
     MISS
