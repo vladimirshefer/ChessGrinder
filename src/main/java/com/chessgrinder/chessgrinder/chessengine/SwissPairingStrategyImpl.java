e8f8bb6173b74912bd3e322d69e581a709c41043
Vladimir Shefer <quameu@gmail.com>
2024-08-06 22:27:23 +0200

    REST: Deprecate SwissPairingStrategyImpl

--- src/main/java/com/chessgrinder/chessgrinder/chessengine/SwissPairingStrategyImpl.java
+++ src/main/java/com/chessgrinder/chessgrinder/chessengine/SwissPairingStrategyImpl.java
@@ -14,6 +14,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 //@Component
+@Deprecated
 @RequiredArgsConstructor
 public class SwissPairingStrategyImpl implements PairingStrategy {
 
