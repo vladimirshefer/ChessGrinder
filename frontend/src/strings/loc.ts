5e1e3fb7c1cee3168afe1de21c47513a8b1047d3
Vladimir Shefer <quameu@gmail.com>
2024-08-06 21:52:21 +0200

    UI: useTransliterate feature

--- frontend/src/strings/loc.ts
+++ frontend/src/strings/loc.ts
@@ -21,3 +21,37 @@ export function useLoc(): (key: string) => string {
         return locales?.[language]?.[key] || locales?.["default"]?.[key] || key;
     }
 }
+
+export function useTransliterate(): (key: string) => string {
+    let [language] = useContext(LanguageContext);
+
+    if (language === "ru") {
+        return (key: string): string => key
+    }
+
+    return (key: string): string => {
+        let result = '';
+
+        for (let i = 0; i < key.length; i++) {
+            const char = key[i];
+            result += translit_ru_en[char] || char;
+        }
+
+        return result;
+    }
+}
+
+const translit_ru_en: { [key: string]: string } = {
+    'А': 'A', 'а': 'a', 'Б': 'B', 'б': 'b', 'В': 'V', 'в': 'v',
+    'Г': 'G', 'г': 'g', 'Д': 'D', 'д': 'd', 'Е': 'E', 'е': 'e',
+    'Ё': 'YO', 'ё': 'yo', 'Ж': 'ZH', 'ж': 'zh', 'З': 'Z', 'з': 'z',
+    'И': 'I', 'и': 'i', 'Й': 'J', 'й': 'j', 'К': 'K', 'к': 'k',
+    'Л': 'L', 'л': 'l', 'М': 'M', 'м': 'm', 'Н': 'N', 'н': 'n',
+    'О': 'O', 'о': 'o', 'П': 'P', 'п': 'p', 'Р': 'R', 'р': 'r',
+    'С': 'S', 'с': 's', 'Т': 'T', 'т': 't', 'У': 'U', 'у': 'u',
+    'Ф': 'F', 'ф': 'f', 'Х': 'H', 'х': 'h', 'Ц': 'C', 'ц': 'c',
+    'Ч': 'CH', 'ч': 'ch', 'Ш': 'SH', 'ш': 'sh', 'Щ': 'SCH', 'щ': 'sch',
+    'Ъ': '', 'ъ': '', 'Ы': 'Y', 'ы': 'y', 'Ь': '', 'ь': '',
+    'Э': 'E', 'э': 'e', 'Ю': 'YU', 'ю': 'yu', 'Я': 'YA', 'я': 'ya',
+}
+
