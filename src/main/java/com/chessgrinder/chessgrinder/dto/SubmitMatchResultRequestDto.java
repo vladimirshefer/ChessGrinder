127b5a0ee7c5ecfef6da6d9c438e47eed15f507b
Vladimir Shefer <quameu@gmail.com>
2024-08-06 23:59:32 +0200

    REST: MyActiveTournament: Submit: looks like just refactoring

--- src/main/java/com/chessgrinder/chessgrinder/dto/SubmitMatchResultRequestDto.java
+++ src/main/java/com/chessgrinder/chessgrinder/dto/SubmitMatchResultRequestDto.java
@@ -1,10 +1,12 @@
 package com.chessgrinder.chessgrinder.dto;
 
 import com.chessgrinder.chessgrinder.enums.*;
+import jakarta.annotation.Nullable;
 import lombok.*;
 
 @Data
 public class SubmitMatchResultRequestDto {
 
+    @Nullable
     MatchResult matchResult;
 }
