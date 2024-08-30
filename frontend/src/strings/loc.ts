a0da8427245585fda44cae5ecf4b1ccd94772ff4
yshefer <yana.shefer.it@gmail.com>
2024-08-30 16:57:36 +0200

    Refactoring

--- frontend/src/strings/loc.ts
+++ frontend/src/strings/loc.ts
@@ -31,26 +31,28 @@ export function useTransliterate(): (key: string) => string {
 
     return (key: string): string => {
         let result = '';
-        let resultChar;
+        let translitedChar;
         for (let i = 0; i < key.length; i++) {
             const char = key[i];
-            resultChar = translit_ru_en.has(char) ? translit_ru_en.get(char) : char;
-            result += resultChar;
+            translitedChar = translit_ru_en[char];
+            result += (translitedChar === undefined) ? char : translitedChar;
         }
+
         return result;
     }
 }
 
-const translit_ru_en = new Map<string, string>([
-    ["А", "A"], ["а", "a"], ["Б", "B"], ["б", "b"], ["В", "V"], ["в", "v"],
-    ["Г", "G"], ["г", "g"], ["Д", "D"], ["д", "d"], ["Е", "E"], ["е", "e"],
-    ["Ё", "Yo"], ["ё", "yo"], ["Ж", "Zh"], ["ж", "zh"], ["З", "Z"], ["з", "z"],
-    ["И", "I"], ["и", "i"], ["Й", "J"], ["й", "j"], ["К", "K"], ["к", "k"],
-    ["Л", "L"], ["л", "l"], ["М", "M"], ["м", "m"], ["Н", "N"], ["н", "n"],
-    ["О", "O"], ["о", "o"], ["П", "P"], ["п", "p"], ["Р", "R"], ["р", "r"],
-    ["С", "S"], ["с", "s"], ["Т", "T"], ["т", "t"], ["У", "U"], ["у", "u"],
-    ["Ф", "F"], ["ф", "f"], ["Х", "H"], ["х", "h"], ["Ц", "C"], ["ц", "c"],
-    ["Ч", "Ch"], ["ч", "ch"], ["Ш", "Sh"], ["ш", "sh"], ["Щ", "Sch"], ["щ", "sch"],
-    ["Ъ", ""], ["ъ", ""], ["Ы", "Y"], ["ы", "y"], ["Ь", ""], ["ь", ""],
-    ["Э", "E"], ["э", "e"], ["Ю", "Yu"], ["ю", "yu"], ["Я", "Ya"], ["я", "ya"]
-    ]);
+const translit_ru_en: { [key: string]: string } = {
+    'А': 'A', 'а': 'a', 'Б': 'B', 'б': 'b', 'В': 'V', 'в': 'v',
+    'Г': 'G', 'г': 'g', 'Д': 'D', 'д': 'd', 'Е': 'E', 'е': 'e',
+    'Ё': 'Yo', 'ё': 'yo', 'Ж': 'Zh', 'ж': 'zh', 'З': 'Z', 'з': 'z',
+    'И': 'I', 'и': 'i', 'Й': 'J', 'й': 'j', 'К': 'K', 'к': 'k',
+    'Л': 'L', 'л': 'l', 'М': 'M', 'м': 'm', 'Н': 'N', 'н': 'n',
+    'О': 'O', 'о': 'o', 'П': 'P', 'п': 'p', 'Р': 'R', 'р': 'r',
+    'С': 'S', 'с': 's', 'Т': 'T', 'т': 't', 'У': 'U', 'у': 'u',
+    'Ф': 'F', 'ф': 'f', 'Х': 'H', 'х': 'h', 'Ц': 'C', 'ц': 'c',
+    'Ч': 'Ch', 'ч': 'ch', 'Ш': 'Sh', 'ш': 'sh', 'Щ': 'Sch', 'щ': 'sch',
+    'Ъ': '', 'ъ': '', 'Ы': 'Y', 'ы': 'y', 'Ь': '', 'ь': '',
+    'Э': 'E', 'э': 'e', 'Ю': 'Yu', 'ю': 'yu', 'Я': 'Ya', 'я': 'ya',
+}
+
