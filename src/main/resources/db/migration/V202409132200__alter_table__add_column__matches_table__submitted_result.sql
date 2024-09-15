f63e5df1106f6a5daa50375d3981c446ba0366fc
Vladimir Shefer <quameu@gmail.com>
2024-09-15 21:48:04 +0200

    REST: HOTFIX: Migration fix

--- src/main/resources/db/migration/V202409132200__alter_table__add_column__matches_table__submitted_result.sql
+++ src/main/resources/db/migration/V202409132200__alter_table__add_column__matches_table__submitted_result.sql
@@ -1,4 +1,4 @@
-ALTER TABLE postgres.public.matches_table
+ALTER TABLE matches_table
     ADD result_submitted_by_participant1 VARCHAR;
-ALTER TABLE postgres.public.matches_table
+ALTER TABLE matches_table
     ADD result_submitted_by_participant2 VARCHAR;
