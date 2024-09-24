0e5fdbdda4583ecc30bcf28b43a3a09f40019eff
Vladimir Shefer <quameu@gmail.com>
2024-09-24 23:48:57 +0200

    REST: H2: BREAKING! Rework migration scripts to work with H2

--- src/main/resources/db/migration/V202403130000__alter_tables__add_auditing_columns.sql
+++ src/main/resources/db/migration/V202403130000__alter_tables__add_auditing_columns.sql
@@ -1,58 +1,48 @@
-ALTER TABLE users_table
-    ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC'),
-    ADD COLUMN updated_at TIMESTAMP,
-    ADD COLUMN created_by VARCHAR(255),
-    ADD COLUMN updated_by VARCHAR(255);
+ALTER TABLE users_table ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC');
+ALTER TABLE users_table ADD COLUMN updated_at TIMESTAMP;
+ALTER TABLE users_table ADD COLUMN created_by VARCHAR(255);
+ALTER TABLE users_table ADD COLUMN updated_by VARCHAR(255);
 UPDATE users_table SET created_by = username;
 
-ALTER TABLE users_roles_table
-    ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC'),
-    ADD COLUMN updated_at TIMESTAMP,
-    ADD COLUMN created_by VARCHAR(255),
-    ADD COLUMN updated_by VARCHAR(255);
+ALTER TABLE users_roles_table ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC');
+ALTER TABLE users_roles_table ADD COLUMN updated_at TIMESTAMP;
+ALTER TABLE users_roles_table ADD COLUMN created_by VARCHAR(255);
+ALTER TABLE users_roles_table ADD COLUMN updated_by VARCHAR(255);
 
-ALTER TABLE user_reputation_history_table
-    ADD COLUMN created_by VARCHAR(255),
-    ADD COLUMN updated_by VARCHAR(255);
+ALTER TABLE user_reputation_history_table ADD COLUMN created_by VARCHAR(255);
+ALTER TABLE user_reputation_history_table ADD COLUMN updated_by VARCHAR(255);
 
-ALTER TABLE users_badges_table
-    ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC'),
-    ADD COLUMN updated_at TIMESTAMP,
-    ADD COLUMN created_by VARCHAR(255),
-    ADD COLUMN updated_by VARCHAR(255);
+ALTER TABLE users_badges_table ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC');
+ALTER TABLE users_badges_table ADD COLUMN updated_at TIMESTAMP;
+ALTER TABLE users_badges_table ADD COLUMN created_by VARCHAR(255);
+ALTER TABLE users_badges_table ADD COLUMN updated_by VARCHAR(255);
 
-ALTER TABLE tournaments_table
-    ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC'),
-    ADD COLUMN updated_at TIMESTAMP,
-    ADD COLUMN created_by VARCHAR(255),
-    ADD COLUMN updated_by VARCHAR(255);
+ALTER TABLE tournaments_table ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC');
+ALTER TABLE tournaments_table ADD COLUMN updated_at TIMESTAMP;
+ALTER TABLE tournaments_table ADD COLUMN created_by VARCHAR(255);
+ALTER TABLE tournaments_table ADD COLUMN updated_by VARCHAR(255);
 
-ALTER TABLE rounds_table
-    ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC'),
-    ADD COLUMN updated_at TIMESTAMP,
-    ADD COLUMN created_by VARCHAR(255),
-    ADD COLUMN updated_by VARCHAR(255);
+ALTER TABLE rounds_table ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC');
+ALTER TABLE rounds_table ADD COLUMN updated_at TIMESTAMP;
+ALTER TABLE rounds_table ADD COLUMN created_by VARCHAR(255);
+ALTER TABLE rounds_table ADD COLUMN updated_by VARCHAR(255);
 
-ALTER TABLE roles_table
-    ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC'),
-    ADD COLUMN updated_at TIMESTAMP,
-    ADD COLUMN created_by VARCHAR(255),
-    ADD COLUMN updated_by VARCHAR(255);
+ALTER TABLE roles_table ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC');
+ALTER TABLE roles_table ADD COLUMN updated_at TIMESTAMP;
+ALTER TABLE roles_table ADD COLUMN created_by VARCHAR(255);
+ALTER TABLE roles_table ADD COLUMN updated_by VARCHAR(255);
 
-ALTER TABLE participants_table
-    ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC'),
-    ADD COLUMN updated_at TIMESTAMP,
-    ADD COLUMN created_by VARCHAR(255),
-    ADD COLUMN updated_by VARCHAR(255);
+ALTER TABLE participants_table ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC');
+ALTER TABLE participants_table ADD COLUMN updated_at TIMESTAMP;
+ALTER TABLE participants_table ADD COLUMN created_by VARCHAR(255);
+ALTER TABLE participants_table ADD COLUMN updated_by VARCHAR(255);
 
-ALTER TABLE matches_table
-    ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC'),
-    ADD COLUMN updated_at TIMESTAMP,
-    ADD COLUMN created_by VARCHAR(255),
-    ADD COLUMN updated_by VARCHAR(255);
+ALTER TABLE matches_table ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC');
+ALTER TABLE matches_table ADD COLUMN updated_at TIMESTAMP;
+ALTER TABLE matches_table ADD COLUMN created_by VARCHAR(255);
+ALTER TABLE matches_table ADD COLUMN updated_by VARCHAR(255);
 
-ALTER TABLE badges_table
-    ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC'),
-    ADD COLUMN updated_at TIMESTAMP,
-    ADD COLUMN created_by VARCHAR(255),
-    ADD COLUMN updated_by VARCHAR(255);
+ALTER TABLE badges_table ADD COLUMN created_at TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'UTC');
+ALTER TABLE badges_table ADD COLUMN updated_at TIMESTAMP;
+ALTER TABLE badges_table ADD COLUMN created_by VARCHAR(255);
+ALTER TABLE badges_table ADD COLUMN updated_by VARCHAR(255);
