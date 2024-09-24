8b63c68c2853f72f59d974ef3432613da51f7784
Vladimir Shefer <quameu@gmail.com>
2024-09-24 23:48:57 +0200

    REST: BREAKING! Rework migration scripts to work with H2

--- src/main/resources/db/migration/V202402130000__alter_table__participants_table__add_constraint.sql
+++ src/main/resources/db/migration/V202402130000__alter_table__participants_table__add_constraint.sql
@@ -6,6 +6,10 @@ ALTER TABLE participants_table
 ALTER TABLE participants_table
     ADD CONSTRAINT fk__participants_table__tournaments_table
         FOREIGN KEY (tournament_id) REFERENCES tournaments_table
-            ON DELETE CASCADE,
-    ADD CONSTRAINT unique_id_tournament_id UNIQUE (id, tournament_id),
-    ALTER COLUMN tournament_id SET NOT NULL;
+            ON DELETE CASCADE;
+
+ALTER TABLE participants_table
+    ADD CONSTRAINT unique_id_tournament_id UNIQUE (id, tournament_id);
+
+ALTER TABLE participants_table
+    ALTER tournament_id SET NOT NULL;
