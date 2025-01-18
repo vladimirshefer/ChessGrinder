c3920f70fbb214823639c8b3bee3c5e88ada5cad
Vladimir Shefer <quameu@gmail.com>
2025-01-18 22:55:52 +0100

    REST: Minor refactoring

--- src/main/java/com/chessgrinder/chessgrinder/repositories/TournamentRepository.java
+++ src/main/java/com/chessgrinder/chessgrinder/repositories/TournamentRepository.java
@@ -1,14 +1,10 @@
 package com.chessgrinder.chessgrinder.repositories;
 
 import com.chessgrinder.chessgrinder.entities.TournamentEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.util.List;
 import java.util.UUID;
 
-public interface TournamentRepository extends PagingAndSortingRepository<TournamentEntity, UUID>, CrudRepository<TournamentEntity, UUID> {
-
-    @Override
-    List<TournamentEntity> findAll();
+public interface TournamentRepository extends PagingAndSortingRepository<TournamentEntity, UUID>, ListCrudRepository<TournamentEntity, UUID> {
 }
