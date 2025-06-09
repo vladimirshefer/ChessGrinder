1428099dbe164b778fe1f1e649f9fa528aaae187
Alexander Boldyrev <al.boldyrev1@gmail.com>
2025-06-09 21:02:26 +0300

    Backend & UI: console.log deleted

--- frontend/src/pages/TournamentPage/index.tsx
+++ frontend/src/pages/TournamentPage/index.tsx
@@ -124,8 +124,8 @@ function TournamentPage() {
     const [setFullResponse] = useState<any>(null);
 
     async function createStrawpoll() {
-        console.log("createStrawpoll called");
-        const response = await restApiClient.get(`/strawpoll/${id}`, { responseType: 'text' });        console.log("Full response:", response);
+
+        const response = await restApiClient.get(`/strawpoll/${id}`, { responseType: 'text' });
         setFullResponse(response);
     }
 
