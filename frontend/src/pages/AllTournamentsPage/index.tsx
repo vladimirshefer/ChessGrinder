237853b9d5f27c466f92518964b44246f279b797
Vladimir Shefer <quameu@gmail.com>
2025-08-26 22:43:38 +0200

    UI: Filters on AllTournamentsPage

--- frontend/src/pages/AllTournamentsPage/index.tsx
+++ frontend/src/pages/AllTournamentsPage/index.tsx
@@ -23,6 +23,7 @@ export default function AllTournamentsPage() {
         .filter(it => searchParams.getAll("status").length === 0 || searchParams.getAll("status").map(it => it.toLowerCase()).includes(it.status?.toLowerCase() || ""))
         .filter(it => searchParams.getAll("city").length === 0 || searchParams.getAll("city").map(it => it.toLowerCase()).includes(it.city?.toLowerCase() || ""))
         .filter(it => searchParams.getAll("name").length === 0 || searchParams.getAll("name").map(it => it.toLowerCase()).find(nameParam => it.name?.toLowerCase().includes(nameParam)) !== undefined)
+        .filter(it => searchParams.get("finished") === null || searchParams.get("finished") !== "false" && searchParams.get("finished") !== "true" || searchParams.get("finished") === "false" && it.status !== "FINISHED" || searchParams.get("finished") === "true" && it.status === "FINISHED")
 
     function createTournament() {
         navigate(`/tournament/create`)
