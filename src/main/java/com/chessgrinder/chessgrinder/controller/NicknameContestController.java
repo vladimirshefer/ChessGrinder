feeca66670c5bb0a200c35025d7843f902312aae
Vladimir Shefer <quameu@gmail.com>
2025-09-24 00:44:21 +0200

    REST

--- src/main/java/com/chessgrinder/chessgrinder/controller/NicknameContestController.java
+++ src/main/java/com/chessgrinder/chessgrinder/controller/NicknameContestController.java
@@ -6,22 +6,21 @@ import com.chessgrinder.chessgrinder.service.*;
 import com.chessgrinder.chessgrinder.util.CacheUtil;
 import lombok.*;
 import org.springframework.http.*;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/strawpoll")
+@RequestMapping("/nickname-contest")
 @RequiredArgsConstructor
-public class StrawpollController {
+public class NicknameContestController {
 
     private final StrawpollService strawpollService;
 
     public static final Map<UUID, String> cachedLinks = CacheUtil.createCache(20);
 
     @PreAuthorize("hasPermission(#tournamentId,'TournamentEntity','MODERATOR')")
-    @GetMapping(value = "/{tournamentId}", produces = MediaType.TEXT_PLAIN_VALUE)
-    public String makeStrawpoll(@PathVariable UUID tournamentId) {
+    @PostMapping(value = "/{tournamentId}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String create(@PathVariable UUID tournamentId) {
         synchronized (cachedLinks) {
             if (cachedLinks.containsKey(tournamentId)) {
                 return cachedLinks.get(tournamentId);
@@ -30,6 +29,14 @@ public class StrawpollController {
             cachedLinks.put(tournamentId, result);
             return result;
         }
+    }
+
+    @GetMapping(value = "/{tournamentId}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String get(@PathVariable UUID tournamentId) {
+        if (cachedLinks.containsKey(tournamentId)) {
+            return cachedLinks.get(tournamentId);
+        }
+        return "No poll created yet.";
     }
 
 }
