0069cf7bd5e87407e6b85d2294a7dcededb86700
Vladimir Shefer <quameu@gmail.com>
2025-10-03 20:18:25 +0200

    UI: Add delimiter between users in user list
    The user list was hard to read because stats were in a separate column,
    making it unclear which stats belonged to which user. Added a delimiter
    to improve readability.

--- frontend/src/pages/MainPage/MemberList.tsx
+++ frontend/src/pages/MainPage/MemberList.tsx
@@ -14,8 +14,12 @@ export function MemberList(
 
     return <div>
         <h2 className={"text-xl my-2 uppercase text-left font-semibold"}>{loc("Members")}</h2>
-        <div className={"w-full grid grid-cols-12"}>
-            {users.map(user => <UserPane key={user.id} user={user}/>)}
+        <div className={"w-full grid"}>
+            {users.map(user =>
+                <div className={"border-b py-1"}>
+                    <UserPane key={user.id} user={user}/>
+                </div>
+            )}
         </div>
     </div>;
 }
