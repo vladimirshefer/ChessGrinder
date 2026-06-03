5d797c3170bd6719072f6b8d0b0b3106d088d82e
Vladimir Shefer <quameu@gmail.com>
2026-06-03 21:02:10 +0200

    UI: Local development proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,6 +19,12 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
   },
   preview: {
     port: 3000,
